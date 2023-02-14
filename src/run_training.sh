ecceeca8c72edd9fcf357880849812cfeaa7a23f
ertozi <abecares001@gmail.com>
2023-02-14 14:21:28 +0100

    Commit test after name change

--- src/run_training.sh
+++ src/run_training.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-nnUNet_plan_and_preprocess -t 999 -pl3d None
+nnUNet_plan_and_preprocess -t 900 -pl3d None
 for i in 0 1 2 3 4
 do
-    nnUNet_train 2d nnUNetTrainerV2 999 $i
+    nnUNet_train 2d nnUNetTrainerV2 900 $i
 done
